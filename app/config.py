d14013395dcb491c7f78954680b6976bbdb232b4
UppercutsFromKupchino <mitya.kalmykov.01@mail.ru>
2022-02-13 21:49:26 +0300

    config

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 class Config:
     dialect = 'postgresql'
-    username = 'postgres'
-    password = 'alp37327'
-    host = 'localhost'
-    db_name = 'Golovnin_BD'
+    username = 'luqubhcondwbtk'
+    password = 'ca410cc9828a8f4d40e711ac20f40df1e7463e1db66b12c84673a562a6dd3893'
+    host = 'ec2-54-76-249-45.eu-west-1.compute.amazonaws.com'
+    db_name = 'd1cq3sm6rep74o'
 
     SQLALCHEMY_DATABASE_URI = f'{dialect}://{username}:{password}@{host}/{db_name}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
